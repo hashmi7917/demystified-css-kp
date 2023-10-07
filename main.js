8ffe2a1666f34ae1af72522432ff4eabd80ba608
hashmi7917 <hmuqtadeer@gmail.com>
2023-10-07 17:11:19 +0530

    desktop version

---
+++ main.js
@@ -0,0 +1,21 @@
+anime({
+  targets: 'header',
+  translateY: [-100, 0],
+  opacity: [0, 1],
+  duration: 3000,
+});
+
+anime({
+  targets: 'aside',
+  translateY: [100, 0],
+  delay: 100,
+  opacity: [0, 1],
+  duration: 3000,
+});
+
+anime({
+  targets: 'nav',
+  translateX: [-200, 0],
+  opacity: [0, 1],
+  duration: 3000,
+});
