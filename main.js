ff7c7e1f7060493710c1867eb0129fcf6e232c8d
hashmi7917 <hmuqtadeer@gmail.com>
2023-10-08 12:18:38 +0530

    sidemenu added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// spring animation
 anime({
   targets: 'header',
   translateY: [-100, 0],
@@ -6,16 +7,17 @@ anime({
 });
 
 anime({
-  targets: 'aside',
+  targets: 'h1',
   translateY: [100, 0],
   delay: 100,
   opacity: [0, 1],
   duration: 3000,
 });
 
-anime({
-  targets: 'nav',
-  translateX: [-200, 0],
-  opacity: [0, 1],
-  duration: 3000,
-});
+function openNav() {
+  document.getElementById('sidemenu').style.width = '250px';
+}
+
+function closeNav() {
+  document.getElementById('sidemenu').style.width = '0';
+}
